190aa7c22c56af644f3776cde190bf5f888016ef
Daniel Sutcliffe <dannysutcliffe@hotmail.com>
2020-03-02 08:58:41 +0000

    Update GitHub

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
-const bodyParser = require ('body-parser'); // get form data
-const cors = require ('cors');              // communicate between ports - Cross Origin Resource Sharing
-const nanoID = require('nanoid');
-const mongoose = require('mongoose');
+// Install Packages
+const express = require ('express');                    // Web application framework
+const cors = require ('cors');                          // Communicate between ports - Cross Origin Resource Sharing
+const bodyParser = require ('body-parser');             // Get form data
+
+const mongoose = require('mongoose');                   // Object modelling tool
 const session = require('express-session');             // This line must come before the following
-const MongoStore = require ('connect-mongo')(session);  // call monogo and pass session into that
-const express = require ('express');        // web application framework
+const MongoStore = require ('connect-mongo')(session);  // Call monogo and pass session into that
+const nanoID = require('nanoid');                       // Unique string ID generator
 
 const app = express();
 
@@ -16,8 +18,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// Provide link to database
 mongoose.connect('mongodb+srv://danjaysut:password1234@usersignup-ruhg9.mongodb.net/test?retryWrites=true&w=majority')
-
 app.use(session({
     store: new MongoStore({
         url: 'mongodb+srv://danjaysut:password1234@usersignup-ruhg9.mongodb.net/test?retryWrites=true&w=majority'
@@ -55,5 +57,3 @@ app.get('/ping', async(req, res) => {
 app.listen(8080, () => {
     console.log('server running on port 8080');
 });
-
-// Everything above is all we need for backend
